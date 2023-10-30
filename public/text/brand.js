7884422e0108e906f992d7bddceab11d63dd4cff
opu183059 <opuctg440@gmail.com>
2023-10-30 13:32:54 +0600

    Icon and Brand name changed for header and footer

--- public/text/brand.js
+++ public/text/brand.js
@@ -3,9 +3,9 @@ module.exports = {
     name: 'Booking Dei',
     desc: 'Booking Dei - A Hotel Management Application for your hotel',
     prefix: 'luxiren',
-    footerText: 'Luxiren Theme 2021',
-    logoText: 'Luxiren Software',
-    projectName: 'Software',
+    footerText: 'Booking Dei 2023',
+    logoText: 'Booking Dei',
+    projectName: 'Booking Dei',
     url: 'luxireact.ux-maestro.com/saas',
     img: '/static/images/saas-logo.png',
     notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
