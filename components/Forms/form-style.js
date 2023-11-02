85891bd5e9dc15f4231dc3063c1af38be69e6071
opu183059 <opuctg440@gmail.com>
2023-11-02 17:08:38 +0600

    Contact page design changed according to figma design, contact form- white background in light mood, transparent background in dark mood

--- components/Forms/form-style.js
+++ components/Forms/form-style.js
@@ -18,10 +18,10 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
       padding: theme.spacing(5, 0)
     },
     [`& .${classes.title}`]: {
-      color: theme.palette.common.white,
+      color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
     },
     '& a': {
-      color: theme.palette.mode === 'dark' ? theme.palette.primary.light : theme.palette.primary.main,
+      color: theme.palette.mode === 'dark' ? theme.palette.primary.dark : theme.palette.primary.main,
       textTransform: 'none',
       fontSize: 16,
       textDecoration: 'none',
@@ -35,7 +35,7 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
     textAlign: 'left',
   },
   fullFromWrap: {
-    color: theme.palette.common.white,
+    color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
     paddingTop: theme.spacing(8),
     [theme.breakpoints.down('sm')]: {
       paddingTop: theme.spacing(4)
@@ -44,14 +44,14 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
   formBox: {
     borderRadius: 40,
     overflow: 'hidden',
-    background: 'transparent',
+    background: theme.palette.mode === 'dark' ? 'transparent' : "#FFFFFF",
     boxShadow: '0 1.5px 12px 2px rgba(0, 0, 0, 0.28)',
     [theme.breakpoints.down('sm')]: {
       boxShadow: 'none'
     }
   },
   desc: {
-    color: theme.palette.common.white,
+    color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
     textAlign: 'center',
     padding: theme.spacing(0, 10),
     [theme.breakpoints.down('sm')]: {
@@ -80,17 +80,17 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
     },
     [`&.${classes.light}`]: {
       '& label': {
-        color: theme.palette.common.white,
+        color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
       },
       '& > div': {
-        border: `1px solid ${alpha(theme.palette.primary.light, 0.5)}`,
+        border: `1px solid ${alpha(theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black, 0.5)}`,
         '& input': {
-          color: theme.palette.common.white,
+          color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
           '&:focus': {
-            background: alpha(theme.palette.text.disabled, 0.2)
+            background: alpha(theme.palette.primary.light, 0.1)
           },
           '&:hover': {
-            background: alpha(theme.palette.text.disabled, 0.2)
+            background: alpha(theme.palette.primary.light, 0.1)
           }
         },
       }
@@ -146,7 +146,7 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
     '& span': {
       '& a': {
         textDecoration: 'none !important',
-        color: theme.palette.secondary.main,
+        color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
       }
     },
     [`&.${classes.flex}`]: {
@@ -225,7 +225,7 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
     },
     '& i': {
       fontSize: 32,
-      color: alpha(theme.palette.common.white, 0.54)
+      color: alpha(theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black, 0.54)
     },
     '& > span i:first-of-type': {
       opacity: 1,
@@ -249,7 +249,7 @@ const contactStyles = makeStyles({ uniqId: 'form' })((theme, _params, classes) =
   },
   check: {
     '& svg': {
-      fill: theme.palette.secondary.main
+      fill: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
     }
   },
   decoration: {
