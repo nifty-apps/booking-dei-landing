508e52cb7502afaa8794ff70eb1fd21f51169c08
alamin-nifty <alaminkhan@niftyitsoultion.com>
2023-12-29 18:30:28 +0600

    dynamic route problem solved

--- next.config.js
+++ next.config.js
@@ -1,24 +1,105 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-unused-labels */
-/* eslint-disable no-labels */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-unused-vars */
-// Enable this code below for Server Side Rendering/Translation (SSR)
-// const { i18n } = require('./next-i18next.config')
+// /* eslint-disable no-param-reassign */
+// /* eslint-disable no-unused-labels */
+// /* eslint-disable no-labels */
+// /* eslint-disable no-restricted-syntax */
+// /* eslint-disable no-unused-vars */
+// // Enable this code below for Server Side Rendering/Translation (SSR)
+// // const { i18n } = require('./next-i18next.config')
+// const withImages = require("next-images");
+// const ESLintPlugin = require("eslint-webpack-plugin");
+// const webpack = require("webpack");
+
+// module.exports = withImages({
+//   // Enable this code below for Server Side Rendering/Translation (SSR)
+//   //  i18n,
+//   trailingSlash: true,
+//   images: {
+//     disableStaticImages: true,
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+//   publicRuntimeConfig: {
+//     localeSubpaths:
+//       typeof process.env.LOCALE_SUBPATHS === "string"
+//         ? process.env.LOCALE_SUBPATHS
+//         : "none",
+//   },
+
+//   webpack: (config, options) => {
+//     cssModules: true;
+
+//     // webpack: (config, options) => {
+//     //   cssModules: true;
+//     //   config.plugins
+//     //     .push
+//     //     //      new ESLintPlugin({
+//     //     //        exclude: ['node_modules']
+//     //     //      })
+//     //     ();
+//     //   config.node = {};
+
+//     //   return config;
+//     // },
+
+//     config.node = {};
+//     config.resolve.fallback = { fs: false };
+//     config.module.rules.push({
+//       test: /\.js$/,
+//       exclude:
+//         /node_modules(?!\/quill-image-drop-module|quill-image-resize-module)/,
+//       loader: "babel-loader",
+//     });
+
+//     config.plugins.push(
+//       new webpack.ProvidePlugin({
+//         "window.Quill": "quill",
+//       })
+//     );
+
+//     return config;
+//   },
+// });
+
+// module.exports = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value:
+//               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
 const withImages = require("next-images");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = withImages({
-  // Enable this code below for Server Side Rendering/Translation (SSR)
-  //  i18n,
   trailingSlash: true,
   images: {
     disableStaticImages: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   publicRuntimeConfig: {
@@ -27,23 +108,7 @@ module.exports = withImages({
         ? process.env.LOCALE_SUBPATHS
         : "none",
   },
-
   webpack: (config, options) => {
-    cssModules: true;
-
-    // webpack: (config, options) => {
-    //   cssModules: true;
-    //   config.plugins
-    //     .push
-    //     //      new ESLintPlugin({
-    //     //        exclude: ['node_modules']
-    //     //      })
-    //     ();
-    //   config.node = {};
-
-    //   return config;
-    // },
-
     config.node = {};
     config.resolve.fallback = { fs: false };
     config.module.rules.push({
@@ -52,13 +117,6 @@ module.exports = withImages({
         /node_modules(?!\/quill-image-drop-module|quill-image-resize-module)/,
       loader: "babel-loader",
     });
-    // it was inside of config function
-    // config.plugins
-    //   .push
-    //   //      new ESLintPlugin({
-    //   //        exclude: ['node_modules']
-    //   //      })
-    //   ();
 
     config.plugins.push(
       new webpack.ProvidePlugin({
@@ -68,16 +126,9 @@ module.exports = withImages({
 
     return config;
   },
-});
-
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
@@ -95,4 +146,4 @@ module.exports = {
       },
     ];
   },
-};
+});
