14efdd7aea6e92236a202c5f446b2d77df20b0a2
alamin-nifty <alaminkhan@niftyitsoultion.com>
2024-01-02 18:25:17 +0600

    dynamic route re defined to fix production problem 2

--- pages/[locale]/blogs-media/[id].js
+++ pages/[locale]/blogs-media/[id].js
@@ -111,115 +111,14 @@
 
 // export default blog;
 
-// import React, { Fragment } from "react";
-// import Header from "~/components/Header";
-// import Footer from "~/components/Footer";
-// import Head from "next/head";
-// import { makeStyles } from "tss-react/mui";
-// import PropTypes from "prop-types";
-// import SingleBlog from "~/components/Blogs/SingleBlog";
-// import { makeStaticProps } from "~/lib/getStatic";
-
-// const useStyles = makeStyles({ uniqId: "singleBlog" })((theme) => ({
-//   header: {
-//     background: theme.palette.primary.dark,
-//     position: "relative",
-//     height: "130px",
-//   },
-//   footer: {
-//     background: theme.palette.primary.dark,
-//     position: "relative",
-//   },
-// }));
-
-// const BlogPage = (props) => {
-//   const { classes } = useStyles();
-//   const { onToggleDark, onToggleDir, blog } = props;
-
-//   if (!blog) {
-//     // Handle case when blog data is not available
-//     return <div>Blog not found.</div>;
-//   }
-
-//   return (
-//     <Fragment>
-//       <Head>
-//         <title>{blog.title}</title>{" "}
-//         {/* Assuming blog object has a title property */}
-//       </Head>
-//       <div className={classes.header}>
-//         <Header onToggleDark={onToggleDark} onToggleDir={onToggleDir} />
-//       </div>
-//       <SingleBlog blog={blog} />
-//       <div className={classes.footer}>
-//         <Footer />
-//       </div>
-//     </Fragment>
-//   );
-// };
-
-// BlogPage.propTypes = {
-//   onToggleDark: PropTypes.func.isRequired,
-//   onToggleDir: PropTypes.func.isRequired,
-//   blog: PropTypes.object.isRequired,
-// };
-
-// export async function getStaticProps(context) {
-//   // Call your custom makeStaticProps function to get common props
-//   const commonProps = await makeStaticProps(["common"])(context);
-
-//   // Fetch data for the blog post with the given ID
-//   let blogData = null;
-//   try {
-//     const response = await fetch(
-//       `http://localhost:3008/api/blogs/${context.params.id}`
-//     );
-//     if (response.ok) {
-//       blogData = await response.json();
-//     } else {
-//       console.error("Blog not found", response.status);
-//     }
-//   } catch (error) {
-//     console.error("Error fetching blog", error);
-//   }
-
-//   // If blog data is not found, return notFound: true to render a 404 page
-//   if (!blogData) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-
-//   // Combine the common props with the blog data
-//   const props = {
-//     ...commonProps.props,
-//     blog: blogData,
-//   };
-
-//   return { props };
-// }
-
-// export async function getStaticPaths() {
-//   // Optionally fetch and list all possible paths here
-//   // For now, we'll just tell Next.js to dynamically generate pages on request
-//   return {
-//     paths: [],
-//     fallback: "blocking",
-//   };
-// }
-
-// export default BlogPage;
-
-import React, { useEffect, useState, Fragment } from "react";
-import { useRouter } from "next/router";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import React, { Fragment } from "react";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
-import SingleBlog from "~/components/Blogs/SingleBlog";
-import { makeStyles } from "tss-react/mui";
 import Head from "next/head";
+import { makeStyles } from "tss-react/mui";
 import PropTypes from "prop-types";
-import { makeStaticProps } from "../../../lib/getStatic";
+import SingleBlog from "~/components/Blogs/SingleBlog";
+import { makeStaticProps } from "~/lib/getStatic";
 
 const useStyles = makeStyles({ uniqId: "singleBlog" })((theme) => ({
   header: {
@@ -233,32 +132,20 @@ const useStyles = makeStyles({ uniqId: "singleBlog" })((theme) => ({
   },
 }));
 
-const BlogPage = ({ onToggleDark, onToggleDir }) => {
+const BlogPage = (props) => {
   const { classes } = useStyles();
-  const [blog, setBlog] = useState(null);
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    if (id) {
-      // Replace 'http://localhost:3008' with your production API endpoint if different
-      fetch(`http://localhost:3008/api/blogs/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setBlog(data);
-        })
-        .catch((error) => console.error("Fetching blog failed", error));
-    }
-  }, [id]);
+  const { onToggleDark, onToggleDir, blog } = props;
 
   if (!blog) {
-    return <div>Loading...</div>;
+    // Handle case when blog data is not available
+    return <div>Blog not found.</div>;
   }
 
   return (
     <Fragment>
       <Head>
-        <title>{blog.title}</title>
+        <title>{blog.title}</title>{" "}
+        {/* Assuming blog object has a title property */}
       </Head>
       <div className={classes.header}>
         <Header onToggleDark={onToggleDark} onToggleDir={onToggleDir} />
@@ -274,16 +161,132 @@ const BlogPage = ({ onToggleDark, onToggleDir }) => {
 BlogPage.propTypes = {
   onToggleDark: PropTypes.func.isRequired,
   onToggleDir: PropTypes.func.isRequired,
+  blog: PropTypes.object.isRequired,
 };
 
-const getStaticProps = makeStaticProps(["common"]);
-// You'll need to export getStaticPaths if you're using i18n and fallback: false
+export async function getStaticProps(context) {
+  // Call your custom makeStaticProps function to get common props
+  const commonProps = await makeStaticProps(["common"])(context);
+
+  // Fetch data for the blog post with the given ID
+  let blogData = null;
+  try {
+    const response = await fetch(
+      `http://localhost:3008/api/blogs/${context.params.id}`
+    );
+    if (response.ok) {
+      blogData = await response.json();
+    } else {
+      console.error("Blog not found", response.status);
+    }
+  } catch (error) {
+    console.error("Error fetching blog", error);
+  }
+
+  // If blog data is not found, return notFound: true to render a 404 page
+  if (!blogData) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // Combine the common props with the blog data
+  const props = {
+    ...commonProps.props,
+    blog: blogData,
+  };
+
+  return { props };
+}
+
 export async function getStaticPaths() {
+  // Optionally fetch and list all possible paths here
+  // For now, we'll just tell Next.js to dynamically generate pages on request
   return {
-    paths: [], // or dynamically generate paths based on your locales
-    fallback: "blocking", // can also be true or false
+    paths: [],
+    fallback: true,
   };
 }
-export { getStaticProps };
 
 export default BlogPage;
+
+// import React, { useEffect, useState, Fragment } from "react";
+// import { useRouter } from "next/router";
+// import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+// import Header from "~/components/Header";
+// import Footer from "~/components/Footer";
+// import SingleBlog from "~/components/Blogs/SingleBlog";
+// import { makeStyles } from "tss-react/mui";
+// import Head from "next/head";
+// import PropTypes from "prop-types";
+// import { getStaticPaths, makeStaticProps } from "../../../lib/getStatic";
+
+// const useStyles = makeStyles({ uniqId: "singleBlog" })((theme) => ({
+//   header: {
+//     background: theme.palette.primary.dark,
+//     position: "relative",
+//     height: "130px",
+//   },
+//   footer: {
+//     background: theme.palette.primary.dark,
+//     position: "relative",
+//   },
+// }));
+
+// const BlogPage = ({ onToggleDark, onToggleDir }) => {
+//   const { classes } = useStyles();
+//   const [blog, setBlog] = useState(null);
+//   const router = useRouter();
+//   const { id } = router.query;
+
+//   useEffect(() => {
+//     if (id) {
+//       // Replace 'http://localhost:3008' with your production API endpoint if different
+//       fetch(`http://localhost:3008/api/blogs/${id}`)
+//         .then((res) => res.json())
+//         .then((data) => {
+//           setBlog(data);
+//           console.log(data);
+//         })
+//         .catch((error) => console.error("Fetching blog failed", error));
+//     }
+//   }, [id]);
+
+//   if (!blog) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <Fragment>
+//       <Head>
+//         <title>{blog.title}</title>
+//       </Head>
+//       <div className={classes.header}>
+//         <Header onToggleDark={onToggleDark} onToggleDir={onToggleDir} />
+//       </div>
+//       <SingleBlog blog={blog} />
+//       <div className={classes.footer}>
+//         <Footer />
+//       </div>
+//     </Fragment>
+//   );
+// };
+
+// BlogPage.propTypes = {
+//   onToggleDark: PropTypes.func.isRequired,
+//   onToggleDir: PropTypes.func.isRequired,
+// };
+
+// const getStaticProps = makeStaticProps(["common"]);
+// export { getStaticProps };
+// const getStaticProps = makeStaticProps(["common"]);
+// You'll need to export getStaticPaths if you're using i18n and fallback: false
+// export async function getStaticPaths() {
+//   return {
+//     paths: [], // or dynamically generate paths based on your locales
+//     fallback: "blocking", // can also be true or false
+//   };
+// }
+// export { getStaticProps };
+
+// export default BlogPage;
